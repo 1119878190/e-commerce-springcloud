1261758c3b7eeb7461c349c7b3fdbd9189d735fe
lx <1119878190@qq.com>
2022-05-24 00:15:07 +0800

    接入 SpringBoot Admin

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # e-commerce-springcloud
+
+## 技术要点
+nacos服务注册与发现
+
+SpringBoot Admin 监控
+
--- e-commerce-alibaba-nacos-client/src/main/java/com/imooc/ecommerce/NacosClientApplication.java
+++ e-commerce-alibaba-nacos-client/src/main/java/com/imooc/ecommerce/NacosClientApplication.java
@@ -4,10 +4,12 @@ package com.imooc.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * Nacos Client 工程启动入口
  */
+@RefreshScope
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosClientApplication {
