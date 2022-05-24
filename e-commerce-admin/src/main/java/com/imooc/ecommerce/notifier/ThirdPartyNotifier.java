5510b110a7ec8af168109b2c1367429fa8ce8cd6
lx <1119878190@qq.com>
2022-05-24 23:22:44 +0800

    SpringBoot Admin 自定义告警通知

---
+++ e-commerce-admin/src/main/java/com/imooc/ecommerce/notifier/ThirdPartyNotifier.java
@@ -0,0 +1,49 @@
+package com.imooc.ecommerce.notifier;
+
+
+import de.codecentric.boot.admin.server.domain.entities.Instance;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
+import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
+import de.codecentric.boot.admin.server.notify.AbstractEventNotifier;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+/**
+ * 自定义告警
+ */
+@Slf4j
+@Component
+public class ThirdPartyNotifier extends AbstractEventNotifier {
+
+
+    protected ThirdPartyNotifier(InstanceRepository repository) {
+        super(repository);
+    }
+
+
+    /**
+     * 实现对时间的通知
+     *
+     * @param event
+     * @param instance
+     * @return
+     */
+    @Override
+    protected Mono<Void> doNotify(InstanceEvent event, Instance instance) {
+
+        return Mono.fromRunnable(() -> {
+            if (event instanceof InstanceStatusChangedEvent) {
+
+                log.info("Instance Status Change: [{}] , [{}], [{}]", instance.getRegistration().getName(), event.getInstance(),
+                        ((InstanceStatusChangedEvent) event).getStatusInfo().getStatus());
+
+            } else {
+                log.info("Instance info : [{}] , [{}], [{}]", instance.getRegistration().getName(), event.getInstance(),
+                        event.getType());
+            }
+        });
+
+    }
+}
